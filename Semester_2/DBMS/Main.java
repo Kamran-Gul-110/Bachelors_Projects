141c6d80bc091f7b719566adf83ddaa4cdc2b8e3
Kamran Gul <kamrangul42142@gmail.com>
2026-03-28 22:00:28 +0500

    Person's Constructor and Admin class added

--- Semester_2/DBMS/Main.java
+++ Semester_2/DBMS/Main.java
@@ -3,4 +3,19 @@ class Person{
     private String lastName;
     private String fatherName;
     private String contact;
+    Person(String firstName,String lastName,String fatherName,String contact){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fatherName = fatherName;
+        this.contact = contact;
+    }
+}
+class Admin extends Person{
+    private int adminId;
+    private String adminPassword;
+    Admin(String firstName,String lastName,String fatherName,String contact,int adminId,String adminPassword){
+        super(firstName,lastName,fatherName,contact);
+        this.adminId = adminId;
+        this.adminPassword = adminPassword;
+    }
 }
